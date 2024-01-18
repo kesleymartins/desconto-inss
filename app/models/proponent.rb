b36eefe8f0c9e894b0ee73629c0303386b9d8ba1
kesleymartins <kesley.martins@vivaldi.net>
2024-01-18 19:47:01 -0300

    set address as proponent dependent destroy

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -2,6 +2,6 @@ class Proponent < ApplicationRecord
   validates :name, presence: true
   validates :cpf, presence: true
 
-  has_one :address
+  has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
 end
