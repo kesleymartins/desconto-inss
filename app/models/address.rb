4712674d4243dc8b996012b6b69f0bba25ce9ae5
kesleymartins <kesley.martins@vivaldi.net>
2024-01-18 18:03:13 -0300

    add model validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+  validates :street, presence: true
+  validates :number, presence: true
+  validates :district, presence: true
+  validates :zip_code, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
 end
--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -1,2 +1,4 @@
 class Proponent < ApplicationRecord
+  validates :name, presence: true
+  validates :cpf, presence: true
 end
