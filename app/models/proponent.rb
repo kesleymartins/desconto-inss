62c9cf4c90b67b339bfbe338104e1ace19b1fff6
kesleymartins <kesley.martins@vivaldi.net>
2024-01-20 03:05:35 -0300

    update salary/discount validation to allow seed without salary

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -1,8 +1,7 @@
 class Proponent < ApplicationRecord
   validates :name, presence: true
   validates :cpf, presence: true
-  validates :salary, presence: true
-  validates :discount, presence: true
+  validates :discount, presence: true, if: proc { |a| a.salary.present? }
 
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
