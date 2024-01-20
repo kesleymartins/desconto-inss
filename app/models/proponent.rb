b74716b8d4a4570f8c83d0666bde764de3379e04
kesleymartins <kesley.martins@vivaldi.net>
2024-01-20 02:51:39 -0300

    add presence validation for salary and discount

--- app/models/proponent.rb
+++ app/models/proponent.rb
@@ -1,6 +1,8 @@
 class Proponent < ApplicationRecord
   validates :name, presence: true
   validates :cpf, presence: true
+  validates :salary, presence: true
+  validates :discount, presence: true
 
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
